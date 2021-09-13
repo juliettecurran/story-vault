f73e1d164e1aee787ed727f45b4c3b72bc0b0084
juliettecurran <juliettecurran@hotmail.com>
2021-09-13 22:52:11 +0100

    Imported components and added routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import './App.css';
-import CharacterList from './Components/CharacterList';
-import NewCharacterForm from './Components/NewCharacterForm';
+import Header from "./Components/Head/Header";
+import Navbar from "./Components/Head/Navbar";
+import Home from "./Components/Content/Home";
+import CharacterList from './Components/Content/CharacterList';
+import NewCharacterForm from './Components/Content/NewCharacterForm';
+import SingleCharacter from './Components/Content/SingleCharacter';
+import Footer from "./Components/Footer/Footer";
 import CharacterStore from './Stores/CharacterStore';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 
 function App() {
 
-  //const store = new CharacterStore();
+  //const store = new CharacterStore(); Router!!!
 
   return (
-    <div>
-      <NewCharacterForm/>
-      <CharacterList/>
+
+    <div className='App'>
+      <BrowserRouter>
+        <div className='Head'>
+          <Header />
+          <Navbar/>
+        </div>
+        <div className='Content'>
+          <Switch>
+          <Route exact path='/'>
+             <Home/>
+            </Route>
+            <Route exact path='/all'>
+              <CharacterList />
+            </Route>
+            <Route exact path='/new'>
+              <NewCharacterForm />
+            </Route>
+            <Route exact path='/character/:review_id'>
+              <SingleCharacter />
+            </Route> 
+          </Switch>
+        </div>
+        <div className='Footer'>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
+
+    {/* <>
+    <Navbar/>
+    <div className="container">
+      <Route exact={true} path="/" component={Home} />
+      <Route exaxt path="/new" component={NewCharacterForm} />
+      <Route exact path="/all" component={CharacterList} />
+    </div>
+    </>
+  );
+} */}
 
 export default App;
