8224ef49d124b083663631d6e633ff5cf619cb95
juliettecurran <juliettecurran@hotmail.com>
2021-09-13 15:17:01 +0100

    imported components to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import CharacterList from './Components/CharacterList';
+import NewCharacterForm from './Components/NewCharacterForm';
+import CharacterStore from './Stores/CharacterStore';
+
 
 function App() {
+
+  //const store = new CharacterStore();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewCharacterForm/>
+      <CharacterList/>
     </div>
   );
 }
