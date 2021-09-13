feed6ca0657e6bfc3ad096de7722cc00d6b51e3b
juliettecurran <juliettecurran@hotmail.com>
2021-09-13 22:54:37 +0100

    Init footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>this is my footer</p>
+        </footer>
+    )
+}
+
+export default Footer
