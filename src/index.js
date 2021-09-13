c00e068e00174b7ec5d873a57d76d2a59201f975
juliettecurran <juliettecurran@hotmail.com>
2021-09-13 22:51:52 +0100

    Removed boilerplate and added Router

--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { CharacterStoreProvider } from './Provider/CharacterStoreProvider';
 import CharacterStore from './Stores/CharacterStore';
 
-
+import { BrowserRouter as  Router } from "react-router-dom";
 const characterStore = new CharacterStore();
 
 ReactDOM.render(
   <React.StrictMode>
+
     <CharacterStoreProvider characterStore={characterStore}>
+      <Router>
       <App />
+      </Router>
     </CharacterStoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
