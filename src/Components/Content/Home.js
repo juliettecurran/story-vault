b6d0c67da106de2a3bb9d87907f2557bd9050d6c
juliettecurran <juliettecurran@hotmail.com>
2021-09-14 12:48:44 +0100

    Added and styled Home text

--- src/Components/Content/Home.js
+++ src/Components/Content/Home.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <p>This is home</p>
+            <h3>This is home</h3>
+            <p className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur, nunc condimentum molestie suscipit, est ex euismod ipsum, id iaculis tortor magna a nibh. Etiam porta sollicitudin iaculis. Nam vitae varius augue. Aenean a orci nisi. Integer eu lacus leo. Proin mattis enim vitae convallis euismod. Nunc volutpat, urna a aliquet ultrices, nulla ex varius tellus, sed tempus quam eros eu lorem. Maecenas tincidunt sollicitudin diam. Nam lectus nibh, molestie ultricies sem ac, interdum facilisis felis. Morbi vitae condimentum turpis, et pulvinar nunc. Mauris quis tincidunt neque, gravida tristique magna. Phasellus faucibus tellus sed hendrerit tempus. Duis imperdiet mattis nisi vel dictum. Etiam eget magna sit amet leo tempus fermentum. Mauris a convallis magna.</p>
         </div>
     )
 }
