d9d5baa6eb12a0b6a46126532bea7dbc6646400e
juliettecurran <juliettecurran@hotmail.com>
2021-09-14 11:03:06 +0100

    Added totalCharacters to Header

--- src/Components/Head/Header.js
+++ src/Components/Head/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useCharacterStore } from '../../Provider/CharacterStoreProvider'
 
 const Header = () => {
+    const totalCharacters = useCharacterStore().totalCharacters
     return (
         <div>
             <p>This is the header</p>
+            <h4> {totalCharacters} character(s) created</h4> {/*how to ternary plural when > 1 */}
         </div>
     )
 }
