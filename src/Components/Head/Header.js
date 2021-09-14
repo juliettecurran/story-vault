2996eb73c31b2edf111f1bf03c71d207196119c3
juliettecurran <juliettecurran@hotmail.com>
2021-09-14 16:49:38 +0100

    Adjusted content

--- src/Components/Head/Header.js
+++ src/Components/Head/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useCharacterStore } from '../../Provider/CharacterStoreProvider'
+import { observer } from 'mobx-react'
 
 const Header = () => {
     const { totalCharacters } = useCharacterStore()
     return (
-        <div>
-            <p>This is the header</p>
+        <header>
+            <h2>Story Vault</h2>
             <h4> {totalCharacters} character(s) created</h4> {/*how to ternary plural when > 1 */}
-        </div>
+        </header>
     )
 }
 
-export default Header
+export default observer(Header) /*Add observer to make header update*/
