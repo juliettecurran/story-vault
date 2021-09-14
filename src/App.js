15304722010f93504c218c335cb652b511ab2e23
juliettecurran <juliettecurran@hotmail.com>
2021-09-14 11:01:30 +0100

    Removed unneeded code and added grid

--- src/App.js
+++ src/App.js
@@ -6,23 +6,24 @@ import CharacterList from './Components/Content/CharacterList';
 import NewCharacterForm from './Components/Content/NewCharacterForm';
 import SingleCharacter from './Components/Content/SingleCharacter';
 import Footer from "./Components/Footer/Footer";
-import CharacterStore from './Stores/CharacterStore';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
-
 function App() {
-
-  //const store = new CharacterStore(); Router!!!
-
   return (
 
-    <div className='App'>
+    
+  
+ 
+  
+
+
+<div className="grid-container App">
+    
       <BrowserRouter>
-        <div className='Head'>
+      <div className="Header">
           <Header />
           <Navbar/>
-        </div>
+          </div>
         <div className='Content'>
           <Switch>
           <Route exact path='/'>
@@ -43,19 +44,9 @@ function App() {
           <Footer />
         </div>
       </BrowserRouter>
-    </div>
+      </div>
+    
   );
 }
-
-    {/* <>
-    <Navbar/>
-    <div className="container">
-      <Route exact={true} path="/" component={Home} />
-      <Route exaxt path="/new" component={NewCharacterForm} />
-      <Route exact path="/all" component={CharacterList} />
-    </div>
-    </>
-  );
-} */}
 
 export default App;
