594c3ce30504d44a55f5c5cba01b465fc9ef1532
juliettecurran <juliettecurran@hotmail.com>
2021-09-15 12:41:02 +0100

    Added flex positioning

--- src/Components/Head/Header.js
+++ src/Components/Head/Header.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import { useCharacterStore } from '../../Provider/CharacterStoreProvider'
-import { observer } from 'mobx-react'
+import React from "react";
+import { useCharacterStore } from "../../Provider/CharacterStoreProvider";
+import { observer } from "mobx-react";
+import "./head.css";
 
 const Header = () => {
-    const { totalCharacters } = useCharacterStore()
-    return (
-        <header>
-            <h2>Story Vault</h2>
-            <h4> {totalCharacters} character(s) created</h4> {/*how to ternary plural when > 1 */}
-        </header>
-    )
-}
+	const { totalCharacters } = useCharacterStore();
+	return (
+		<header className='CharacterList__flexContainer'>
+			<h2 className='CharacterList__flexItem'>Story Vault</h2>
+			<h4 className='CharacterList__flexItem'>
+				{totalCharacters} character(s) created
+			</h4>
 
-export default observer(Header) /*Add observer to make header update*/
+			{/*how to ternary plural when > 1 */}
+		</header>
+	);
+};
+
+export default observer(Header); /*Add observer to make header update*/
