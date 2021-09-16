352437d6eaeca6c9a1b11917b27969d3eb35a18b
juliettecurran <juliettecurran@hotmail.com>
2021-09-16 22:45:32 +0100

    Fixed bug w/ totalCharacter evaluation on refresh

--- src/Stores/CharacterStore.js
+++ src/Stores/CharacterStore.js
@@ -1,59 +1,69 @@
-import { makeAutoObservable } from 'mobx';
-import Character from '../Models/Character';
+import { makeAutoObservable } from "mobx";
+import Character from "../Models/Character";
+import {
+	setSessionValue,
+	getSessionValue,
+	removeSessionValue,
+} from "../helpers";
 
 class CharacterStore {
-     /* ------- init characterList array ------- */
-	characterList = [];
-    totalCharacters = 0;
-    characterDetails = null;
-    createCharacter = null;
-    updateCharacter = null;
-    deleteCharacter = null;
-    characterById = null;
+	/* ------- init characterList array ------- */
 
-    /* ------- observe array and methods ------- */
-    constructor() {
-        makeAutoObservable(this);       
-    }
+	characterList = getSessionValue("characterList") || [];
+	totalCharacters =
+		(getSessionValue("characterList") &&
+			getSessionValue("characterList").length) ||
+		0;
+	characterDetails = null;
 
+	/* ------- observe array and methods ------- */
+	constructor() {
+		makeAutoObservable(this);
+	}
 
-    /* ------- methods ------- */
-	 createCharacter = (character) => {
-        const newCharacter = new Character(character)
-		 this.characterList.push(newCharacter); 
-         this.totalCharacters = this.characterList.length
-	} 
-  
-	updateCharacter(characterId, update) {
+	/* ------- methods ------- */
+	createCharacter = (character) => {
+		const newCharacter = new Character(character);
+		this.characterList.push(newCharacter);
+		this.totalCharacters = this.characterList.length;
+		setSessionValue("characterList", this.characterList);
+	};
+
+	updateCharacter = (characterId, updatedData) => {
 		const characterIndexAtId = this.characterList.findIndex(
 			(character) => character.Id === characterId
 		);
-		if (characterIndexAtId >= 0 && update) {
-			this.characterList[characterIndexAtId] =
-				update;
+		if (characterIndexAtId >= 0 && updatedData) {
+			this.characterList[characterIndexAtId] = updatedData;
 		}
-	}
+		setSessionValue("characterList", this.characterList);
+	};
 
-    deleteCharacter(characterId) {
-        const characterIndexAtId = this.characterList.findIndex((character) => character.Id === characterId);
-        if (characterIndexAtId > -1) {
-            this.characterList.splice(characterIndexAtId, 1)
-            this.totalCharacters = this.characterList.length
-        }
-    }
+	deleteCharacter = (characterId) => {
+		const characterIndexAtId = this.characterList.findIndex(
+			(character) => character.Id === characterId
+		);
+		if (characterIndexAtId > -1) {
+			this.characterList.splice(characterIndexAtId, 1);
+			this.totalCharacters = this.characterList.length;
+		}
+		setSessionValue("characterList", this.characterList);
+	};
 
-     characterById(characterId) {
-        const characterIndexAtId = this.characterList.findIndex((character) => character.Id === characterId);
-        return this.characterList[characterIndexAtId]
-    }
-  
-     characterDetails() {
-        return `You have ${this.totalCharacters} characters`
-    }
+	characterById = (characterId) => {
+		const characterIndexAtId = this.characterList.findIndex(
+			(character) => character.Id === characterId
+		);
+		return this.characterList[characterIndexAtId];
+	};
 
-    logCharacterDetails() {
-        console.log(this.characterDetails)
-    }
+	characterDetails = () => {
+		return `You have ${this.totalCharacters} characters`;
+	};
+
+	logCharacterDetails = () => {
+		console.log(this.characterDetails);
+	};
 }
 
 export default CharacterStore;
