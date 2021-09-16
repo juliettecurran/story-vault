434086ff02b0914dc081795defd70e537d272762
juliettecurran <juliettecurran@hotmail.com>
2021-09-16 23:33:15 +0100

    Created Location model

---
+++ src/Models/Location.js
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from "uuid";
+
+class Location {
+	constructor(data) {
+		this.Id = uuidv4();
+		this.name = data.name;
+		this.demonym = data.demonym;
+		this.size = data.size;
+		this.ruler = data.ruler;
+		this.population = data.population;
+		this.geography = data.geography;
+		this.culturalTrait = data.culturalTrait;
+		this.dominantReligion = data.dominantReligion;
+		this.language = data.language;
+		this.symbol = data.symbol;
+	}
+}
+
+export default Location;
