b89f66dc6ebb2a50ece3af5cc5293ae29ece29a2
juliettecurran <juliettecurran@hotmail.com>
2021-09-16 22:43:34 +0100

    Extracted out form components and styling

---
+++ src/Components/Content/Form/FormElements/FormSelectField.js
@@ -0,0 +1,21 @@
+import React from "react";
+import characterSelectOptions from "../../../../characterOptionsData";
+import Label from "../Label";
+
+const FormSelectField = ({ name, value, onChange, label, options }) => {
+	const typeOptions = characterSelectOptions[options];
+	return (
+		<>
+			<Label name={name} label={label} />
+			<select name={name} value={value} onChange={onChange}>
+				{typeOptions.map((item, index) => (
+					<option key={index} value={item}>
+						{item}
+					</option>
+				))}
+			</select>
+		</>
+	);
+};
+
+export default FormSelectField;
