162dc4dbcffb38f5d03184b3b30173206c24f100
juliettecurran <juliettecurran@hotmail.com>
2021-09-16 22:58:12 +0100

    Init Location form component

---
+++ src/Components/Content/Form/NewLocationForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ToggleFormType from "./ToggleFormType";
+
+const NewLocationForm = () => {
+	return (
+		<div>
+			<ToggleFormType />
+		</div>
+	);
+};
+
+export default NewLocationForm;
