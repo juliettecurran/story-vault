e5a5ff42a0a6404b86083b64ba2a758c11305b04
juliettecurran <juliettecurran@hotmail.com>
2021-09-16 22:51:04 +0100

    Init obj array of character form select options

---
+++ src/characterOptionsData.js
@@ -0,0 +1,25 @@
+export const characterSelectOptions = {
+	characterType: [
+		"Protagonist",
+		"Antagonist",
+		"Love Interest",
+		"Mentor",
+		"Friend",
+		"Side-Character",
+	],
+
+	characterLocation: [
+		"Ocean",
+		"Mountain",
+		"Town",
+		"Mainland",
+		"Orphanage",
+		"Other",
+	],
+
+	characterEyeColour: ["Blue", "Brown", "Green", "Other"],
+
+	characterHairColour: ["Brunette", "Blonde", "Ginger", "Black", "White"],
+};
+
+export default characterSelectOptions;
