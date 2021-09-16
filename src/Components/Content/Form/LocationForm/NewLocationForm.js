7a6ca49a66fd6bfb33d39fe5018a8c9c209a6506
juliettecurran <juliettecurran@hotmail.com>
2021-09-16 23:20:52 +0100

    Moved into subfolder

---
+++ src/Components/Content/Form/LocationForm/NewLocationForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ToggleFormType from "../FormElements/ToggleFormType.js";
+
+const NewLocationForm = () => {
+	return (
+		<div>
+			<ToggleFormType />
+		</div>
+	);
+};
+
+export default NewLocationForm;
