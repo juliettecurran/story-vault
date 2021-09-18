f2afb127096787133bc7e25362eec27b800b9b79
juliettecurran <juliettecurran@hotmail.com>
2021-09-18 01:28:41 +0100

    Added defaultState and handlers

--- src/Components/Content/Form/LocationForm/NewLocationForm.js
+++ src/Components/Content/Form/LocationForm/NewLocationForm.js
@@ -1,12 +1,49 @@
 import React from "react";
 import ToggleFormType from "../FormElements/ToggleFormType.js";
+import LocationForm from "./LocationForm.js";
+import { useLocationStore } from "../../../../Provider/LocationStoreProvider";
 
 const NewLocationForm = () => {
+	const locationStore = useLocationStore();
+
+	const defaultState = {
+		name: "",
+		demonym: "",
+		size: "",
+		ruler: "",
+		population: "",
+		geography: "",
+		culturalTrait: "",
+		dominantReligion: "",
+		language: "",
+		symbol: "",
+	};
+
+	const [location, setLocation] = React.useState(defaultState);
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		setLocation({
+			...location,
+			[e.target.name]: value,
+		});
+	};
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		locationStore.createLocation(location);
+		setLocation(defaultState);
+	}
+
 	return (
-		<div>
+		<>
 			<ToggleFormType />
-		</div>
+			<LocationForm
+				location={location}
+				handleChange={handleChange}
+				handleSubmit={handleSubmit}
+			/>
+		</>
 	);
 };
-
 export default NewLocationForm;
