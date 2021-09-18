bf149746938e2e5cc62a4e41031ea4a598a0c137
juliettecurran <juliettecurran@hotmail.com>
2021-09-18 01:29:19 +0100

    Modified form to fit location props

--- src/Components/Content/Form/LocationForm/LocationForm.js
+++ src/Components/Content/Form/LocationForm/LocationForm.js
@@ -1,110 +1,103 @@
 import React from "react";
+import "./locationForm.css";
 import FormSelectField from "../FormElements/FormSelectField";
 import FormTextInput from "../FormElements/FormTextInput";
 import Form from "../Form.js";
 
-const LocationForm = ({ character, handleChange, handleSubmit }) => {
-	return <div>test</div>;
+const LocationForm = ({ location, handleChange, handleSubmit }) => {
+	return (
+		<Form handleSubmit={handleSubmit}>
+			<h3>Create a new location</h3>
+			<hr></hr>
+			<FormTextInput
+				label='Name'
+				type='text'
+				name='name'
+				value={location.name}
+				placeholder='Lord'
+				onChange={handleChange}
+			/>
+
+			<FormTextInput
+				label='Demonym'
+				type='text'
+				name='demonym'
+				value={location.demonym}
+				placeholder='British'
+				onChange={handleChange}
+			/>
+
+			<FormTextInput
+				label='Size'
+				type='text'
+				name='size'
+				value={location.size}
+				placeholder='Large'
+				onChange={handleChange}
+			/>
+
+			<FormTextInput
+				label='Ruler'
+				type='text'
+				name='ruler'
+				value={location.ruler}
+				placeholder='Parliament'
+				onChange={handleChange}
+			/>
+			<FormTextInput
+				label='Population'
+				type='text'
+				name='population'
+				value={location.population}
+				placeholder='1000'
+				onChange={handleChange}
+			/>
+			<FormTextInput
+				label='Geography'
+				type='text'
+				name='geography'
+				value={location.geography}
+				placeholder='Desert'
+				onChange={handleChange}
+			/>
+			<FormTextInput
+				label='Cultural Trait'
+				type='text'
+				name='culturalTrait'
+				value={location.culturalTrait}
+				placeholder='Stoic'
+				onChange={handleChange}
+			/>
+			<FormTextInput
+				label='Dominant Religion'
+				type='text'
+				name='dominantReligion'
+				value={location.dominantReligion}
+				placeholder='Atheism'
+				onChange={handleChange}
+			/>
+			<FormTextInput
+				label='Language'
+				type='text'
+				name='language'
+				value={location.language}
+				placeholder='English'
+				onChange={handleChange}
+			/>
+			<FormTextInput
+				label='Symbol'
+				type='text'
+				name='symbol'
+				value={location.symbol}
+				placeholder='5 pointed star'
+				onChange={handleChange}
+			/>
+
+			<button type='submit' className='submitBtn'>
+				Submit
+			</button>
+		</Form>
+	);
 };
 
 export default LocationForm;
-
-{
-	/* <Form handleSubmit={handleSubmit}>
-        <FormTextInput
-            label='Title'
-            type='text'
-            name='title'
-            value={character.title}
-            placeholder='Lord'
-            onChange={handleChange}
-        />
-
-        <FormTextInput
-            label='Name'
-            type='text'
-            name='name'
-            value={character.name}
-            placeholder='James Marshall'
-            onChange={handleChange}
-        />
-
-        <FormTextInput
-            label='Age'
-            type='number'
-            name='age'
-            value={character.age}
-            placeholder='20'
-            onChange={handleChange}
-            min={1}
-            max={100}
-        />
-
-        <FormSelectField
-            name='characterType'
-            label='Character Type'
-            type='select'
-            value={character.characterType}
-            onChange={handleChange}
-            options='characterType'
-        />
-
-        <FormTextInput
-            label='Occupation'
-            type='text'
-            name='occupation'
-            value={character.occupation}
-            placeholder='Pirate'
-            onChange={handleChange}
-        />
-
-        <FormSelectField
-            name='location'
-            label='Location'
-            type='select'
-            value={character.location}
-            onChange={handleChange}
-            options='characterLocation'
-        />
-
-        <FormSelectField
-            name='eyeColour'
-            label='Eye Colour'
-            type='select'
-            value={character.eyeColour}
-            onChange={handleChange}
-            options='characterEyeColour'
-        />
-
-        <FormSelectField
-            name='hairColour'
-            label='Hair Colour'
-            type='select'
-            value={character.hairColour}
-            onChange={handleChange}
-            options='characterHairColour'
-        />
-
-        <FormTextInput
-            label='Main Trait'
-            type='text'
-            name='mainTrait'
-            value={character.mainTrait}
-            placeholder='Intelligent'
-            onChange={handleChange}
-        />
-
-        <FormTextInput
-            label='Special Ability'
-            type='text'
-            name='specialAbility'
-            value={character.specialAbility}
-            placeholder='None'
-            onChange={handleChange}
-        />
-
-        <button type='submit' className='submitBtn'>
-            Submit
-        </button>
-    </Form> */
-}
