3cb2d162bbba46937c7d576c2797b6c1083ee71c
juliettecurran <juliettecurran@hotmail.com>
2021-09-18 01:19:35 +0100

    Added LocationStoreProvider

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { CharacterStoreProvider } from './Provider/CharacterStoreProvider';
-import CharacterStore from './Stores/CharacterStore';
-
-import { BrowserRouter as  Router } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { CharacterStoreProvider } from "./Provider/CharacterStoreProvider";
+import CharacterStore from "./Stores/CharacterStore";
+import LocationStore from "./Stores/LocationStore";
+import { BrowserRouter as Router } from "react-router-dom";
+import LocationStoreProvider from "./Provider/LocationStoreProvider";
 const characterStore = new CharacterStore();
+const locationStore = new LocationStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-
-    <CharacterStoreProvider characterStore={characterStore}>
-      <Router>
-      <App />
-      </Router>
-    </CharacterStoreProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<LocationStoreProvider locationStore={locationStore}>
+			<CharacterStoreProvider characterStore={characterStore}>
+				<Router>
+					<App />
+				</Router>
+			</CharacterStoreProvider>
+		</LocationStoreProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
