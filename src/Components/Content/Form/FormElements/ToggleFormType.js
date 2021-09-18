cbd80502cf2ee59fe007104dd5355f8e96a2ed38
juliettecurran <juliettecurran@hotmail.com>
2021-09-18 01:29:48 +0100

    Imported styling

--- src/Components/Content/Form/FormElements/ToggleFormType.js
+++ src/Components/Content/Form/FormElements/ToggleFormType.js
@@ -1,12 +1,13 @@
 import React from "react";
 import NavButton from "../../../Head/Navbar/NavButton";
 import { useHistory } from "react-router-dom";
+import "./toggleFormType.css";
 
 const ToggleFormType = () => {
 	let history = useHistory();
 	const activePath = history.location.pathname;
 	return (
-		<div>
+		<div className='formToggle'>
 			<NavButton to='/new/character' active={activePath}>
 				New Character
 			</NavButton>
