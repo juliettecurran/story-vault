9bfaf3ff04c5783d867d47fa408395e139c152d9
juliettecurran <juliettecurran@hotmail.com>
2021-09-18 01:21:12 +0100

    Added locationCount in header

--- src/Components/Head/Header/Header.js
+++ src/Components/Head/Header/Header.js
@@ -1,21 +1,35 @@
 import React from "react";
 import { useCharacterStore } from "../../../Provider/CharacterStoreProvider";
+import { useLocationStore } from "../../../Provider/LocationStoreProvider";
 import { observer } from "mobx-react";
 import "./head.css";
 import { pluralise } from "../../../Utils/helpers.js";
+import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
 	const { totalCharacters } = useCharacterStore();
+	const { totalLocations } = useLocationStore();
+
 	const characterCount = `${totalCharacters} ${pluralise(
 		totalCharacters,
 		"character",
 		"characters"
 	)} created`;
-	console.log(totalCharacters);
+
+	const locationCount = `${totalLocations} ${pluralise(
+		totalLocations,
+		"location",
+		"locations"
+	)} created`;
+
 	return (
 		<header className='CharacterList__flexContainer'>
 			<h2 className='CharacterList__flexItem'>Story Vault</h2>
-			<h4 className='CharacterList__flexItem'>{characterCount}</h4>
+			<Navbar className='CharacterList__flexItem' />
+			<ul className='CharacterList__flexItem'>
+				<li>{characterCount}</li>
+				<li>{locationCount}</li>
+			</ul>
 		</header>
 	);
 };
